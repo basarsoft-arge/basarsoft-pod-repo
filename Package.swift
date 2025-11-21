03bd81463b10d5d4438eb04f9492b7d21a606fa6
Furkan Parlak <furkan.parlak@basarsoft.com.tr>
2025-11-21 19:36:31 +0300

    2.6.3

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "YolbilMobileSDK",
-            url: "https://artifactory.basarsoft.com.tr/artifactory/generic-release-local/pods/yolbil/YolbilMobileSDK-SPM/2.6.2/YolbilMobileSDK.xcframework.zip",
+            url: "https://artifactory.basarsoft.com.tr/artifactory/generic-release-local/pods/yolbil/YolbilMobileSDK-SPM/2.6.3/YolbilMobileSDK.xcframework.zip",
             checksum: "0346cf3803ef89dfc9258d1d2b7d4a0869998403683248edacaf95dcb1b79fb0"
         ),
         
